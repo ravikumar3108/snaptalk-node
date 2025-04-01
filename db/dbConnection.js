0fad945f59100ee7eb4ccb4f56598df919b597cc
ravikumar <raviluhaniwal318@gmail.com>
2025-04-01 21:23:56 +0530

    deploy

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectToMongodb = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL,{
+            family : 4 ,
+        })
         console.log("connection to mongodb succesfull")
     }catch(err){
         console.log(`Error in connecting to Mongodb ${err.message}`)
--- models/userModel.js
+++ models/userModel.js
@@ -26,5 +26,5 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = new mongoose.model("User", userSchema)
+const User =  mongoose.model("User", userSchema)
 export default User;
